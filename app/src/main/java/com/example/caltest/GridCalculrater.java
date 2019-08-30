66a0a4592bf813ed5fe83a6c0709dd8ce113c83a
ParkSongSal <tkdanr8326@daum.net>
2019-08-30 18:01:56 +0900

    GridLayout 배치

---
+++ app/src/main/java/com/example/caltest/GridCalculrater.java
@@ -0,0 +1,14 @@
+package com.example.caltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridCalculrater extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_calculrater);
+    }
+}
